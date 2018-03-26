bc1d9d0eececa98d2aec0376c675af8e6fe6d3df
Robert K. Pauls <r.pauls@robertkpauls.com>
2018-03-26 22:55:49 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,104 @@
+module.exports = function(grunt) {
+
+   grunt.initConfig({
+        watch: {
+            css: {
+                files: 'src/scss/**/*.scss',
+                tasks: ['sass']
+              }
+        },
+        myth: {
+            compile: {
+                expand: true,
+                cwd: 'css',
+                src: ['*.css', '!*.min.css'],
+                dest: 'css',
+                ext: '.css'
+            },
+            release: {
+                files: {
+                    'dist/scifiui.css': 'build/css/scifi-ui-framework.css',
+                    'dist/style.css': 'build/css/style.css'
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'build/css/scifi-ui-framework.css': 'src/scss/scifi-ui-framework.scss',
+                    'build/css/style.css': 'src/scss/style.scss'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'index.html',
+                        'build/css/*.css'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        },
+        cssmin: {
+            concat: {
+                files: {
+                    'dist/scifiui.css': ['build/css/scifi-ui-framework.css'],
+                    'dist/style.css': ['node_modules/flex-responsive-grid/dist/frg.css', 'build/css/style.css']
+                }
+            },
+            minify: {
+                expand: true,
+                cwd: 'css',
+                src: ['*.css', '!*.min.css'],
+                dest: 'css',
+                ext: '.min.css'
+            },
+            release: {
+                expand: true,
+                cwd: 'dist',
+                src: ['*.css', '!*.min.css'],
+                dest: 'dist',
+                ext: '.min.css'
+            }
+        },
+        processhtml: {
+            dist: {
+                options: {
+                    process: true
+                },
+                files: {
+                    'index.html': ['src/index.html']
+                }
+            }
+        },
+    });
+
+    // Grunt plugins
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-myth');
+    grunt.loadNpmTasks('grunt-processhtml');
+    
+    // Default task
+    grunt.registerTask('default', [
+        'sass',
+        'myth',
+        'cssmin:concat',
+        'cssmin:minify',
+        'cssmin:release',
+        'processhtml'
+    ]);
+
+    // BrowserSync with scss watcher task
+    grunt.registerTask('sync', ['browserSync', 'watch']);
+};
